058631db096da4e4dac5ce67349554ec9f07b8d2
Fuad <44843101+FuadIbrahimli@users.noreply.github.com>
2018-12-20 01:05:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+$('document').ready(function(){
+	$("div").next().slideUp()
+	$('.first').click(function(){
+		$("div").next().slideUp()
+		$('.firstLi').find("ul").toggle(1000)
+	})
+},
+
+$('.second').click(function(){
+		$("div").next().slideUp()
+		$('.secondLi').find("ul").toggle(1000)
+	}),
+	
+	$('.third').click(function(){
+		$("div").next().slideUp()
+		$('.thirdLi').find("ul").toggle(1000)
+	}),
+	
+	$('.fourth').click(function(){
+		$("div").next().slideUp()
+		$('.fourthLi').find("ul").toggle(1000)
+	})
+	
+	
+	
+	
+
+
+
+)
